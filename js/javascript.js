3f3b7e214dd7251f17663f9a24d3ad4e7209d41f
Facundo Martínez <facundo.martinez.fms@gmail.com>
2022-10-21 15:08:58 -0300

    registro

---
+++ js/javascript.js
@@ -0,0 +1,36 @@
+const nombre = document.getElementById("name")
+const apellido = document.getElementById("apellido")
+const usuario = document.getElementById("usuario")
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
+const País = document.getElementById("País")
+const ciudad = document.getElementById("ciudad")
+const fec_nac = document.getElementById("fec_nac")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+    if(pass.value.length < 8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
+})
