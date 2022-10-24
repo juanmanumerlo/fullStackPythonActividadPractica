9990ef83fb2acc8ac9bc0f9b432ad1ac3f7a1079
Juan Manuel Merlo <juanmanuelmerlo13@gmail.com>
2022-10-24 00:56:20 -0300

    Agregar menu hamburguesa

--- js/index.js
+++ js/index.js
@@ -1,49 +1,8 @@
-let root={
-    data:function(){
-        return{
-            url:'https://api.sampleapis.com/recipes/recipes',
-            listaReceta:[],
-            completo:false
-        }
-    },
-    methods:{
-        clickSelector:function($event){
-            console.log($event.target.innerHTML)
-            this.value = $event.target.innerHTML
-        },
-        getData:function(){
-            fetch(this.url)
-            .then(resp => resp.json())
-            .then(data => this.listaReceta = data)
-            .catch(e => console.log(e))
-        },
-        getDataASYNC: async function(){
-            try{
-                let data = await fetch(this.url);
-                let dataFinal = await data.json();
-                this.listaReceta = dataFinal;
-                this.completo = !this.completo
-            }catch(e){
-                console.log(e)
-            }
-        },
-        getDataAXIOS: async function(){
-            try {
-                let res = await axios(this.url);
-                this.listaReceta = res.data;
-                console.log(res);
-                this.completo = !this.completo 
-                
-            } catch (error) {
-                console.log(error);
-            }
-        }
+import hamburgerMenu from "./menu_hamburgesa.js";
 
-    },
-    created(){
-        this.getDataAXIOS()
-    }
+//Activar Menu Hamburguesa
+const d = document;
 
-}
-
-let appRuth = Vue.createApp(root).mount('#ruth');
+d.addEventListener("DOMContentLoaded", (e) => {
+    hamburgerMenu(".panel-btn", ".panel", ".menu a");
+});
