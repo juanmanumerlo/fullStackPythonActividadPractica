f0274a80e8403e7807a332300303dc497ea5bc0c
Juan Manuel Merlo <juanmanuelmerlo13@gmail.com>
2022-10-25 16:21:08 -0300

    Agregando carrousel al index

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 import hamburgerMenu from "./menu_hamburgesa.js";
+import slider from "./carrousel.js";
 
-//Activar Menu Hamburguesa
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
+    slider();
 });
