e368753ebe34b8a5047d7e71071da0a8fe0b19f8
Juan Manuel Merlo <juanmanuelmerlo13@gmail.com>
2022-10-26 01:10:01 -0300

    Modificar carrousel en index

--- js/carrousel.js
+++ js/carrousel.js
@@ -10,26 +10,26 @@ export default function slider(){
     d.addEventListener("click", e =>{
         if(e.target === $prevBtn){
             e.preventDefault();
-            $slides[i].classList.remove(" active");
+            $slides[i].classList.remove("active");
             i--;
 
             if(i < 0){
                 i = $slides.length -1;
             }
 
-            $slides[i].classList.add(" active");
+            $slides[i].classList.add("active");
         }
 
         if(e.target === $nextBtn){
             e.preventDefault();
-            $slides[i].classList.remove(" active");
+            $slides[i].classList.remove("active");
             i++;
 
             if(i >= $slides.length){
                 i = 0;
             }
 
-            $slides[i].classList.add(" active");
+            $slides[i].classList.add("active");
         }
     });
 }
